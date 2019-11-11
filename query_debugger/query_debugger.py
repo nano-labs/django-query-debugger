75d986a154559226f43e0a6342c098a8f0e31ea1
Fabio Pacheco <fabio.pacheco@complyadvantage.com>
2019-11-11 17:13:37 +0000

    Add max_depth

--- query_debugger/query_debugger.py
+++ query_debugger/query_debugger.py
@@ -15,8 +15,9 @@ def cprint(string, color):
     print("\033[9%sm%s\033[0m" % (color_code, string))
 
 
-def add_logger(func, only_here=None, everywhere=False):
+def add_logger(func, only_here=None, everywhere=False, max_depth=3):
     u"""Adiciona o print ao método."""
+
     @wraps(func)
     def logger(*args, **kwargs):
         u"""Printa de modo amigável todos os requests feitos."""
@@ -32,7 +33,7 @@ def add_logger(func, only_here=None, everywhere=False):
         indenting = ""
         code_point = inspect.currentframe().f_back
         tracks = []
-        while len(tracks) < 10:
+        while len(tracks) < max_depth:
             code_point = code_point.f_back
             if not code_point:
                 break
@@ -45,19 +46,19 @@ def add_logger(func, only_here=None, everywhere=False):
             if _file_path not in [i for i, j in tracks]:
                 tracks = [(_file_path, _file_line_number)] + tracks
 
-        # if a file is specified but not present then to nothing
-        if only_here and not any(p == only_here for p, l in tracks):
-            return func(*args, **kwargs)
+        if max_depth:
+            # if a file is specified but not present then to nothing
+            if only_here and not any(p for p, l in tracks if str(p) == str(only_here)):
+                return func(*args, **kwargs)
+            if (
+                all(p.startswith("/") and not p.startswith(TOP_DIR) for p, l in tracks)
+                and not everywhere
+            ):
+                return func(*args, **kwargs)
 
-        if all(
-            p.startswith("/") and not _file_path.startswith(TOP_DIR)
-            for p, l in tracks
-        ) and not everywhere:
-            return func(*args, **kwargs)
-
-        for a, l in tracks:
-            cprint("%s%s Line: %s" % (indenting, a, l), "gray")
-            indenting += "  "
+            for a, l in tracks:
+                cprint("%s%s Line: %s" % (indenting, a, l), "gray")
+                indenting += "  "
 
         cprint("%s%s" % (indenting, request_line), "red")
         return func(*args, **kwargs)
@@ -70,16 +71,16 @@ logged_func = add_logger(func)
 setattr(CursorWrapper, "_execute_with_wrappers", logged_func)
 
 
-def here():
+def here(max_depth=3):
     """Only show queries trigged by the file where this import sits."""
     frame = inspect.stack()[1]
     module = inspect.getmodule(frame[0])
     filename = module.__file__
-    logged_func = add_logger(func, only_here=filename)
+    logged_func = add_logger(func, only_here=filename, max_depth=max_depth)
     setattr(CursorWrapper, "_execute_with_wrappers", logged_func)
 
 
-def everywhere():
+def everywhere(max_depth=3):
     """Show queries trigged by django os external libraries too."""
-    logged_func = add_logger(func, everywhere=True)
+    logged_func = add_logger(func, everywhere=True, max_depth=max_depth)
     setattr(CursorWrapper, "_execute_with_wrappers", logged_func)
